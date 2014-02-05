22e8bbcdb7cf433dce867b76cb5efe637384d52d
Sander Jansen <s.jansen@gmail.com>
2014-02-04 20:21:03 -0600

    add back context menu for tags

--- src/GMDatabaseSource.cpp
+++ src/GMDatabaseSource.cpp
@@ -263,12 +263,12 @@ FXbool GMDatabaseSource::getTrack(GMTrack & info) const{
   return db->getTrack(current_track,info);
   }
 
-FXbool GMDatabaseSource::genre_context_menu(FXMenuPane * /*pane*/) {
+FXbool GMDatabaseSource::genre_context_menu(FXMenuPane * pane) {
   //new GMMenuCommand(pane,fxtr("Edit…\tF2\tEdit Genre."),GMIconTheme::instance()->icon_edit,this,GMDatabaseSource::ID_EDIT_GENRE);
 //  new GMMenuCommand(pane,"Export" … "\t\tCopy associated tracks to destination.",GMIconTheme::instance()->icon_export,this,ID_EXPORT_GENRE);
 //  new FXMenuSeparator(pane);
-  //new GMMenuCommand(pane,fxtr("Remove…\tDel\tRemove Genre from Library."),GMIconTheme::instance()->icon_delete,this,GMSource::ID_DELETE_TAG);
-  return false;
+  new GMMenuCommand(pane,fxtr("Remove…\tDel\tRemove Tag from Library."),GMIconTheme::instance()->icon_delete,this,GMSource::ID_DELETE_TAG);
+  return true;
   }
 
 FXbool GMDatabaseSource::artist_context_menu(FXMenuPane * pane){
