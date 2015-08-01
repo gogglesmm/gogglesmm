1fdc87e5724dea9fd8a23ac8bc3c8753455a3646
Sander Jansen <s.jansen@gmail.com>
2015-07-31 22:24:23 -0500

    Use static_cast

--- src/GMPlayListSource.cpp
+++ src/GMPlayListSource.cpp
@@ -94,14 +94,14 @@ void GMPlayListSource::save(GMTrackList* tracklist) {
       items.no(tracklist->getNumItems());
       if (tracklist->getSortMethod()==HEADER_QUEUE){
         for (FXint i=0;i<tracklist->getNumItems();i++){
-          const GMDBTrackItem * trk = dynamic_cast<GMDBTrackItem*>(tracklist->getItem(i));
+          const GMDBTrackItem * trk = static_cast<const GMDBTrackItem*>(tracklist->getItem(i));
           items[i].queue = trk->getTrackQueue();
           items[i].track = trk->getId();
           }
         }
       else {
         for (FXint i=0;i<tracklist->getNumItems();i++){
-          const GMDBTrackItem * trk = dynamic_cast<GMDBTrackItem*>(tracklist->getItem(i));
+          const GMDBTrackItem * trk = static_cast<const GMDBTrackItem*>(tracklist->getItem(i));
           items[i].queue = i+1;
           items[i].track = trk->getId();
           }
